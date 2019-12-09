78546555048ba94246188443bc86ed96258e573e
MohamadJamil <47405520+MohamadJamil@users.noreply.github.com>
2019-12-08 20:08:46 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RECAPTCHA_PUBLIC_KEY='6LcCmMQUAAAAAO6SKhd8XIaipQqTQkJThW2up2WA'
+    RECAPTCHA_PRIVATE_KEY='6LcCmMQUAAAAAPOpqMWXD2cSLhGjt0OBbc-nBgPa'
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
